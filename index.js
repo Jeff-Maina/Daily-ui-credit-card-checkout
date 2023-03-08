516e67d26228e302eddeed9a8b26d873f79a1d84
Jeff <jeffgichuki68@gmail.com>
2023-03-09 00:38:59 +0300

    almost done

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function handleCardNum(value,elem){
+    if(value === 1){
+        document.getElementById("part-one").textContent = elem.value
+    }
+    if(value === 2){
+        document.getElementById("part-2").textContent = elem.value
+    }
+    if(value === 3){
+        document.getElementById("part-3").textContent = elem.value
+    }
+    if(value === 4){
+        document.getElementById("part-4").textContent = elem.value
+    }
+}
+
+function handleCardOwner(elem){
+    document.getElementById("card-holder-name").textContent = elem.value
+}
+
+function handleExpiration(text,elem){
+    if(text === "year"){
+        document.getElementById("expire-year").textContent = elem.value.slice(-2)
+    }
+    else if(text === "month"){
+        document.getElementById("expire-month").textContent = elem.value+ '/'
+    }
+}
+
